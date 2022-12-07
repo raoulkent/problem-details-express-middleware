8bdf20299396c97bdf48a0691fe2328770fa741e
raoulkent <raoulkent@gmail.com>
2022-12-07 15:33:05 +0100

    add random error to GET in health handler

--- code/src/lib/handle-health-request.js
+++ code/src/lib/handle-health-request.js
@@ -1,11 +1,21 @@
+const failsSometimes = () =>
+  new Promise((resolve, reject) => {
+    const random = Math.random();
+    if (random > 0.5) {
+      resolve();
+    }
+    reject(new Error('Random error'));
+  });
+
 exports.getSomething = (requestBody, logger) => {
   let calledWith;
   try {
+    failsSometimes();
     calledWith = JSON.stringify(requestBody);
   } catch (e) {
     calledWith = 'empty';
     logger.error(e.message);
   }
   logger.info(`handle-health-request called with ${calledWith}`);
-  return ({ status: 'OK' });
+  return { status: 'OK' };
 };
